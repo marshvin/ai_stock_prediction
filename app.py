e8d005e21689cabf4ad7d472387aa13e825e834a
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2023-11-23 14:34:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import streamlit as st
 
 def main():
     st.title('Stock Prediction App')
-    
+    # Button for fetching prediction data (replace with actual prediction logic)
+    if st.button('Generate Predictions'):
+        # Replace this with your actual prediction logic
+        # predictions = your_prediction_function(data)
+        # data['Predictions'] = predictions
+        st.success('Predictions generated successfully!')
   
 if __name__ == '__main__':
     main()
