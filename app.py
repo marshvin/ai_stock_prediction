68fe0e91bc3cedd07aa00e9387be1df84f208284
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2023-11-23 14:26:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,9 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from data_loader import load_stock_data  # Import the data loading function
 
-# Load your stock data
-data = load_stock_data()
 
-# Set yfinance configuration to return data without timezone localization
-import yfinance as yf
-yf.pdr_override()
-
-# Function to plot the data
-def plot_stock_data(selected_year):
-    # Filter data for the selected year
-    filtered_data = data[data.index.year == selected_year]
-
-    # Plot the data
-    plt.figure(figsize=(16, 6))
-    plt.title('Stock Prediction for Year {}'.format(selected_year))
-    plt.xlabel('Date', fontsize=18)
-    plt.ylabel('Close Price USD ($)', fontsize=18)
-    plt.plot(filtered_data['Close'], label='Actual Close Price')
-    
-    # Add predicted values (modify this part based on your data structure)
-    if 'Predictions' in data.columns:
-        predictions = data[data.index.year == selected_year]['Predictions']
-        plt.plot(predictions, label='Predicted Close Price')
-
-    plt.legend(loc='lower right')
-    st.pyplot(plt)  # Show the plot in Streamlit
-
-# Streamlit app
 def main():
     st.title('Stock Prediction App')
     
-    # Button for fetching prediction data (replace with actual prediction logic)
-    if st.button('Generate Predictions'):
-        # Replace this with your actual prediction logic
-        # predictions = your_prediction_function(data)
-        # data['Predictions'] = predictions
-        st.success('Predictions generated successfully!')
-
-    # Input selection for choosing a year
-    selected_year = st.selectbox('Select a Year', sorted(data.index.year.unique()))
-
-    # Display the stock data plot based on the selected year
-    plot_stock_data(selected_year)
-
+  
 if __name__ == '__main__':
     main()
