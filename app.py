1394f63546a3204fb2a8d8ba4c3366de522a2f42
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2023-11-23 14:50:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Function to generate sample data for demonstration
+def generate_sample_data():
+    np.random.seed(42)
+    date_rng = pd.date_range(start='2022-01-01', end='2022-12-31', freq='B')
+    data = pd.DataFrame(date_rng, columns=['date'])
+    data['Close'] = np.random.uniform(low=100, high=200, size=(len(date_rng),))
+    return data
+
+# Function to generate predictions (replace with your actual prediction logic)
+def generate_predictions(data):
+    predictions = np.random.uniform(low=100, high=200, size=(len(data),))
+    return predictions
 
 def main():
     st.title('Stock Prediction App')
-    # Button for fetching prediction data (replace with actual prediction logic)
+
+    # Button for fetching prediction data
     if st.button('Generate Predictions'):
-        # Replace this with your actual prediction logic
-        # predictions = your_prediction_function(data)
-        # data['Predictions'] = predictions
+        # Generate sample data
+        data = generate_sample_data()
+
+        # Generate predictions
+        predictions = generate_predictions(data)
+        data['Predictions'] = predictions
+
+        # Plot the data
+        st.pyplot(plot_predictions(data))
+
         st.success('Predictions generated successfully!')
-  
+
+# Function to plot predictions
+def plot_predictions(data):
+    fig, ax = plt.subplots(figsize=(16, 6))
+    ax.plot(data['date'], data['Close'], label='Actual Close')
+    ax.plot(data['date'], data['Predictions'], label='Predictions')
+    ax.set_title('Stock Predictions')
+    ax.set_xlabel('Date', fontsize=18)
+    ax.set_ylabel('Close Price USD ($)', fontsize=18)
+    ax.legend(loc='lower right')
+    return fig
+
 if __name__ == '__main__':
     main()
