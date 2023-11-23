9915078d5a1d7e411a1d7c879b66f6271df0c8ab
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2023-11-23 15:47:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,12 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Function to generate sample data for demonstration
-def generate_sample_data():
-    np.random.seed(42)
-    date_rng = pd.date_range(start='2022-01-01', end='2022-12-31', freq='B')
-    data = pd.DataFrame(date_rng, columns=['date'])
-    data['Close'] = np.random.uniform(low=100, high=200, size=(len(date_rng),))
-    return data
-
-# Function to generate predictions (replace with your actual prediction logic)
-def generate_predictions(data):
-    predictions = np.random.uniform(low=100, high=200, size=(len(data),))
-    return predictions
 
 def main():
     st.title('Stock Prediction App')
 
-    # Button for fetching prediction data
-    if st.button('Generate Predictions'):
-        # Generate sample data
-        data = generate_sample_data()
-
-        # Generate predictions
-        predictions = generate_predictions(data)
-        data['Predictions'] = predictions
-
-        # Plot the data
-        st.pyplot(plot_predictions(data))
-
-        st.success('Predictions generated successfully!')
-
-# Function to plot predictions
-def plot_predictions(data):
-    fig, ax = plt.subplots(figsize=(16, 6))
-    
-    # Convert 'date' column to a list
-    dates = data['date'].astype(str).tolist()
-    
-    ax.plot(dates, data['Close'], label='Actual Close')
-    ax.plot(dates, data['Predictions'], label='Predictions')
-    
-    ax.set_title('Stock Predictions')
-    ax.set_xlabel('Date', fontsize=18)
-    ax.set_ylabel('Close Price USD ($)', fontsize=18)
-    ax.legend(loc='lower right')
-    
-    return fig
-
+    # Button for displaying the saved image
+    if st.button('Show Predictions'):
+        # Display the saved image
+        st.image('model_predictions.png', use_column_width=True)
 
 if __name__ == '__main__':
     main()
