70a172ce0b7fc0d76d705bfdc789b8942cfa910f
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2023-11-23 14:24:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 from data_loader import load_stock_data  # Import the data loading function
 
 # Load your stock data
-# Load your stock data
 data = load_stock_data()
 
-# Remove timezone information from the index
-data.index = data.index.tz_localize(None)
+# Set yfinance configuration to return data without timezone localization
+import yfinance as yf
+yf.pdr_override()
 
-# Rest of your code remains the same...
 # Function to plot the data
 def plot_stock_data(selected_year):
     # Filter data for the selected year
@@ -31,4 +30,22 @@ def plot_stock_data(selected_year):
     plt.legend(loc='lower right')
     st.pyplot(plt)  # Show the plot in Streamlit
 
-# Rest of your Streamlit app code remains the same...
+# Streamlit app
+def main():
+    st.title('Stock Prediction App')
+    
+    # Button for fetching prediction data (replace with actual prediction logic)
+    if st.button('Generate Predictions'):
+        # Replace this with your actual prediction logic
+        # predictions = your_prediction_function(data)
+        # data['Predictions'] = predictions
+        st.success('Predictions generated successfully!')
+
+    # Input selection for choosing a year
+    selected_year = st.selectbox('Select a Year', sorted(data.index.year.unique()))
+
+    # Display the stock data plot based on the selected year
+    plot_stock_data(selected_year)
+
+if __name__ == '__main__':
+    main()
