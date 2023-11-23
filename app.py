07400c2093335324c165e3621afbcc80ccc4eb9c
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2023-11-23 14:15:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,10 +6,14 @@ from data_loader import load_stock_data  # Import the data loading function
 # Load your stock data
 data = load_stock_data()
 
+# Remove timezone information from the index
+data.index = data.index.tz_localize(None)
+
+# Rest of your code remains the same...
 # Function to plot the data
 def plot_stock_data(selected_year):
     # Filter data for the selected year
-    filtered_data = data[data['Year'] == selected_year]
+    filtered_data = data[data.index.year == selected_year]
 
     # Plot the data
     plt.figure(figsize=(16, 6))
@@ -21,28 +24,10 @@ def plot_stock_data(selected_year):
     
     # Add predicted values (modify this part based on your data structure)
     if 'Predictions' in data.columns:
-        predictions = data[data['Year'] == selected_year]['Predictions']
+        predictions = data[data.index.year == selected_year]['Predictions']
         plt.plot(predictions, label='Predicted Close Price')
 
     plt.legend(loc='lower right')
     st.pyplot(plt)  # Show the plot in Streamlit
 
-# Streamlit app
-def main():
-    st.title('Stock Prediction App')
-    
-    # Button for fetching prediction data (replace with actual prediction logic)
-    if st.button('Generate Predictions'):
-        # Replace this with your actual prediction logic
-        # predictions = your_prediction_function(data)
-        # data['Predictions'] = predictions
-        st.success('Predictions generated successfully!')
-
-    # Input selection for choosing a year
-    selected_year = st.selectbox('Select a Year', sorted(data['Year'].unique()))
-
-    # Display the stock data plot based on the selected year
-    plot_stock_data(selected_year)
-
-if __name__ == '__main__':
-    main()
+# Rest of your Streamlit app code remains the same...
